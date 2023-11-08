3f556b4b2111dbcb7eda4518bab611b243321943
khrisdain <samuelilesanmi15@gmail.com>
2023-11-08 17:32:33 +0100

    Into to java

---
+++ src/FirstClass.java
@@ -0,0 +1,11 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        //Operator Precedence and Operator challenge
+       double myFirstValue = 20.00d;
+       double mySecondValue = 80.00d;
+       double myTotalValue = myFirstValue + mySecondValue * 100.00d; //operation precedence allows multiplication to start first hence 80 * 100 + 20
+        System.out.println("My total vale is " + myTotalValue);
+
+        double modulusOperator =
+    }
+}
