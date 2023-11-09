2cb31a3387321302e4c354d86a880c059ef927f8
khrisdain <samuelilesanmi15@gmail.com>
2023-11-09 23:01:49 +0100

    today's PR

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,11 +1,14 @@
 public class FirstClass {
     public static void main(String[] args) {
         //Operator Precedence and Operator challenge
-       double myFirstValue = 20.00d;
-       double mySecondValue = 80.00d;
-       double myTotalValue = myFirstValue + mySecondValue * 100.00d; //operation precedence allows multiplication to start first hence 80 * 100 + 20
-        System.out.println("My total vale is " + myTotalValue);
+        double kilometers = ( 100 * 1.609344); //expression
 
-        double modulusOperator =
+        int highScore = 50;
+        if(highScore > 25) {
+            highScore = 1000 + highScore;
+        }
+        System.out.println(highScore);
     }
 }
+
+//Resting today
